25b5143a7fd88a0338acfe14f05025fbecdf21b3
Natasha Ho <natasha.ho@canonical.com>
2022-01-28 21:27:03 -0500

    add test for relations and update github integrate workflow

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -8,7 +8,7 @@ import yaml
 from pytest_operator.plugin import OpsTest
 
 METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
-APP_NAME = "oidc-gatekeeper"
+APP_NAME = METADATA["name"]
 OIDC_CONFIG = {
     "client-name": "Ambassador Auth OIDC",
     "client-secret": "oidc-client-secret",
@@ -26,13 +26,30 @@ async def test_build_and_deploy(ops_test: OpsTest):
     resources = {"oci-image": image_path}
 
     await ops_test.model.deploy(
-        charm_under_test,
-        resources=resources,
-        application_name=APP_NAME,
-        trust=True,
-        config=OIDC_CONFIG,
+        charm_under_test, resources=resources, trust=True, config=OIDC_CONFIG
     )
     await ops_test.model.wait_for_idle(
         apps=[APP_NAME], status="active", raise_on_blocked=True, timeout=60 * 10
     )
     assert ops_test.model.applications[APP_NAME].units[0].workload_status == "active"
+
+
+@pytest.mark.abort_on_fail
+async def test_relations(ops_test: OpsTest):
+    istio_pilot = "istio-pilot"
+    dex_auth = "dex-auth"
+    await ops_test.model.deploy(istio_pilot, channel="1.5/stable")
+    await ops_test.model.deploy(dex_auth)
+    await ops_test.model.add_relation(istio_pilot, dex_auth)
+    await ops_test.model.add_relation(f"{istio_pilot}:ingress", f"{APP_NAME}:ingress")
+    await ops_test.model.add_relation(
+        f"{istio_pilot}:ingress-auth", f"{APP_NAME}:ingress-auth"
+    )
+
+    await ops_test.model.wait_for_idle(
+        [APP_NAME, istio_pilot, dex_auth],
+        status="active",
+        raise_on_blocked=True,
+        raise_on_error=True,
+        timeout=600,
+    )
