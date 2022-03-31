c4621535c507fd21f450cde94c4390802f687afc
Pedro Guimaraes <pedro.guimaraes@canonical.com>
2022-03-31 13:21:52 +0200

    Add support to set custom CA and define auth URLs to be skipped

--- tests/unit/test_operator.py
+++ tests/unit/test_operator.py
@@ -104,3 +104,65 @@ def test_public_url_keep_existing_protocol(harness):
         pod_spec["containers"][0]["envConfig"]["OIDC_PROVIDER"]
         == "https://10.64.140.43.nip.io/dex"
     )
+
+
+def test_skip_auth_url_config_has_value(harness):
+    harness.set_leader(True)
+    harness.add_oci_resource(
+        "oci-image",
+        {
+            "registrypath": "ci-test",
+            "username": "",
+            "password": "",
+        },
+    )
+    harness.update_config({"skip-auth-urls": "/test/,/path1/"})
+    harness.begin_with_initial_hooks()
+
+    pod_spec, _ = harness.get_pod_spec()
+
+    assert (
+        pod_spec["containers"][0]["envConfig"]["SKIP_AUTH_URLS"]
+        == "/dex/,/test/,/path1/"
+    )
+
+
+def test_skip_auth_url_config_is_empty(harness):
+    harness.set_leader(True)
+    harness.add_oci_resource(
+        "oci-image",
+        {
+            "registrypath": "ci-test",
+            "username": "",
+            "password": "",
+        },
+    )
+    harness.begin_with_initial_hooks()
+
+    pod_spec, _ = harness.get_pod_spec()
+
+    assert (
+        pod_spec["containers"][0]["envConfig"]["SKIP_AUTH_URLS"]
+        == "/dex/"
+    )
+
+
+def test_ca_bundle_config(harness):
+    harness.set_leader(True)
+    harness.add_oci_resource(
+        "oci-image",
+        {
+            "registrypath": "ci-test",
+            "username": "",
+            "password": "",
+        },
+    )
+    harness.update_config({"ca-bundle": "aaa"})
+    harness.begin_with_initial_hooks()
+
+    pod_spec, _ = harness.get_pod_spec()
+
+    assert (
+        pod_spec["containers"][0]["envConfig"]["CA_BUNDLE"]
+        == "/etc/certs/oidc/root-ca.pem"
+    )
