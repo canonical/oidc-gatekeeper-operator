c6fb942d60b89673f98f5b237e7de1faf3778317
Orfeas Kourkakis <orfeas.kourkakis@canonical.com>
2024-02-15 12:53:20 +0200

    Add integration tests
    * Assert that oidc charm goes to `Maintenance: Workload failed health
      check` if workload has hit health checks failure threshold
    * Bump charms in testing to use `latest/edge`
    * Add required relation for dex-auth to be reachable
    * Assert that it goes to `Active` once it is configured with proper
      public_url

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -2,9 +2,13 @@
 # See LICENSE file for licensing details.
 from pathlib import Path
 
+import lightkube
 import pytest
 import yaml
+from charmed_kubeflow_chisme.testing import fire_update_status_to_unit
+from lightkube.resources.core_v1 import Service
 from pytest_operator.plugin import OpsTest
+from tenacity import sleep
 
 METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
 APP_NAME = METADATA["name"]
@@ -13,8 +17,9 @@ OIDC_CONFIG = {
     "client-secret": "oidc-client-secret",
 }
 ISTIO_PILOT = "istio-pilot"
+ISTIO_GATEWAY = "istio-gateway"
 DEX_AUTH = "dex-auth"
-PUBLIC_URL = "test-url"
+FALSE_PUBLIC_URL = "test-url"
 image_path = METADATA["resources"]["oci-image"]["upstream-source"]
 RESOURCES = {"oci-image": image_path}
 
@@ -40,33 +45,59 @@ class TestOIDCOperator:
             pytest.charm_under_test, resources=RESOURCES, trust=True, config=OIDC_CONFIG
         )
 
-        await ops_test.model.applications[APP_NAME].set_config({"public-url": PUBLIC_URL})
+        await ops_test.model.applications[APP_NAME].set_config({"public-url": FALSE_PUBLIC_URL})
 
         await ops_test.model.wait_for_idle(
             apps=[APP_NAME], status="active", raise_on_blocked=False, timeout=60 * 10
         )
         assert ops_test.model.applications[APP_NAME].units[0].workload_status == "active"
 
+        # Fire update-status every 10 seconds and expect oidc to go to Maintenance
+        # Didn't use `wait_for_idle()` here because although the unit and apps were
+        # in maintenance, it kept waiting.
+        async with ops_test.fast_forward(fast_interval="10s"):
+            await ops_test.model.block_until(
+                lambda: ops_test.model.applications[APP_NAME].status == "maintenance",
+                timeout=1000,
+            )
+        assert ops_test.model.applications[APP_NAME].units[0].workload_status == "maintenance"
+
     @pytest.mark.abort_on_fail
     async def test_relations(self, ops_test: OpsTest):
-        await ops_test.model.deploy(ISTIO_PILOT, channel="1.16/stable", trust=True)
-        await ops_test.model.deploy(DEX_AUTH, channel="2.31/stable", trust=True)
-        await ops_test.model.add_relation(ISTIO_PILOT, DEX_AUTH)
+        await ops_test.model.deploy(ISTIO_PILOT, channel="latest/edge", trust=True)
+        await ops_test.model.deploy(
+            ISTIO_GATEWAY, channel="latest/edge", trust=True, config={"kind": "ingress"}
+        )
+        await ops_test.model.deploy(DEX_AUTH, channel="latest/edge", trust=True)
+        await ops_test.model.add_relation(
+            f"{ISTIO_PILOT}:istio-pilot", f"{ISTIO_GATEWAY}:istio-pilot"
+        )
         await ops_test.model.add_relation(f"{ISTIO_PILOT}:ingress", f"{APP_NAME}:ingress")
         await ops_test.model.add_relation(
             f"{ISTIO_PILOT}:ingress-auth", f"{APP_NAME}:ingress-auth"
         )
         await ops_test.model.add_relation(f"{APP_NAME}:oidc-client", f"{DEX_AUTH}:oidc-client")
+        await ops_test.model.add_relation(f"{ISTIO_PILOT}:ingress", f"{DEX_AUTH}:ingress")
 
-        await ops_test.model.applications[DEX_AUTH].set_config({"public-url": PUBLIC_URL})
-
-        # Not raising on blocked will allow istio-pilot to be deployed
-        # without istio-gateway and provide oidc with the data it needs.
         await ops_test.model.wait_for_idle(
-            [APP_NAME, ISTIO_PILOT, DEX_AUTH],
+            [ISTIO_PILOT, ISTIO_GATEWAY],
             status="active",
-            raise_on_blocked=False,
-            raise_on_error=True,
+            timeout=600,
+        )
+
+        public_url = get_public_url(ops_test.model_name)
+        await ops_test.model.applications[DEX_AUTH].set_config({"public-url": public_url})
+        await ops_test.model.applications[APP_NAME].set_config({"public-url": public_url})
+
+        # Sleep 45 to allow OIDC workload container to have hit the health check failure
+        # threshold after being reconfigured. Then fire an update-status to set the charm
+        # status and ensure its workload is working.
+        sleep(45)
+        unit_name = f"{APP_NAME}/0"
+        fire_update_status_to_unit(unit_name, ops_test.model_name)
+
+        await ops_test.model.wait_for_idle(
+            status="active",
             timeout=600,
         )
 
@@ -86,7 +117,7 @@ class TestOIDCOperator:
         then refresh, then scale up newly deployed app.
         See https://github.com/juju/juju/pull/15701 for more info.
         """
-        print(f"Deploy {APP_NAME} from stable channel")
+        print(f"Deploy {APP_NAME} from 1.7/stable channel")
         await ops_test.model.deploy(
             APP_NAME, channel="ckf-1.7/stable", trust=True, config=OIDC_CONFIG
         )
@@ -95,14 +126,12 @@ class TestOIDCOperator:
             f"{ISTIO_PILOT}:ingress-auth", f"{APP_NAME}:ingress-auth"
         )
         await ops_test.model.add_relation(f"{APP_NAME}:oidc-client", f"{DEX_AUTH}:oidc-client")
-        await ops_test.model.applications[APP_NAME].set_config({"public-url": PUBLIC_URL})
+        public_url = get_public_url(ops_test.model_name)
+        await ops_test.model.applications[APP_NAME].set_config({"public-url": public_url})
 
         print("Stable charm is deployed, add relations")
         await ops_test.model.wait_for_idle(
-            [APP_NAME, ISTIO_PILOT, DEX_AUTH],
             status="active",
-            raise_on_blocked=False,
-            raise_on_error=True,
             timeout=600,
         )
         print(f"Scale {APP_NAME} to 0 units")
@@ -126,9 +155,22 @@ class TestOIDCOperator:
         print(f"Scale {APP_NAME} to 1 unit")
         await ops_test.model.applications[APP_NAME].scale(scale=1)
 
+        sleep(45)
+        unit_name = f"{APP_NAME}/2"
+        fire_update_status_to_unit(unit_name, ops_test.model_name)
         await ops_test.model.wait_for_idle(
             status="active",
             raise_on_blocked=True,
             raise_on_error=True,
             timeout=1200,
         )
+
+
+def get_public_url(namespace: str):
+    """Extracts public url from service istio-ingressgateway-workload."""
+    lightkube_client = lightkube.Client()
+    ingressgateway_svc = lightkube_client.get(
+        Service, "istio-ingressgateway-workload", namespace=namespace
+    )
+    public_url = f"http://{ingressgateway_svc.status.loadBalancer.ingress[0].ip}"
+    return public_url
