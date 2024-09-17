2310366b2ff95d413426fae1d95a3bcb74787146
misohu <michal.hucko@canonical.com>
2024-09-17 13:13:19 +0200

    Add teraform module

---
+++ terraform/outputs.tf
@@ -0,0 +1,18 @@
+output "app_name" {
+  value = juju_application.oidc_gatekeeper.name
+}
+
+output "provides" {
+  value = {
+    oidc_client  = "oidc-client",
+  }
+}
+
+output "requires" {
+  value = {
+    dex_oidc_config = "dex-oidc-config",
+    ingress = "ingress",
+    ingress_auth = "ingress-auth"
+    logging = "logging"
+  }
+}
