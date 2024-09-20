beaf29bc4033973ce33c964c8625947bec6e45c6
Michal Hucko <michal.hucko@canonical.com>
2024-09-20 14:15:13 +0200

    Merge pull request #175 from canonical/KF-6224-create-tf-module
    Add teraform module

---
+++ terraform/outputs.tf
@@ -0,0 +1,24 @@
+output "app_name" {
+  value = juju_application.oidc_gatekeeper.name
+}
+
+output "peers" {
+  value = {
+    client_secret = "client-secret",
+  }
+}
+
+output "provides" {
+  value = {
+    oidc_client = "oidc-client",
+  }
+}
+
+output "requires" {
+  value = {
+    dex_oidc_config = "dex-oidc-config",
+    ingress         = "ingress",
+    ingress_auth    = "ingress-auth"
+    logging         = "logging"
+  }
+}
