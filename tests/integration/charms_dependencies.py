00d65b0a211d4f0ef1cb69e7ec6e2391e1f97904
Orfeas Kourkakis <orfeas.kourkakis@canonical.com>
2025-06-30 11:00:39 +0300

    refactor(tests): Move dependency charms to a separate file (#204)
    * Move dependency charms deployed during tests to a separate file called
    charms_dependencies.py. This enables programmatic access to them and
    thus updating them centrally, probably with the use of `kfcicli`. This
    uses the `CharmSpec` dataclass from chisme.
    
    Ref canonical/bundle-kubeflow/issues/1256
    
    * chore: bump chisme to 0.4.11
    
    * fix(tests): Update upgrade test
    
    Update test to refresh directly, instead of scaling down and up since
    this was a leftover from podSpec->sidecar upgrades.

---
+++ tests/integration/charms_dependencies.py
@@ -0,0 +1,6 @@
+"""Charms dependencies for tests."""
+
+from charmed_kubeflow_chisme.testing import CharmSpec
+
+DEX_AUTH = CharmSpec(charm="dex-auth", channel="latest/edge", trust=True)
+ISTIO_PILOT = CharmSpec(charm="istio-pilot", channel="latest/edge", trust=True)
